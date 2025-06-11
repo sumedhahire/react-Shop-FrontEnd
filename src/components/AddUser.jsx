a19878524355fd7518fbeea06219f966f1e7acaf
sumedh <ahiresumedh40@gmail.com>
2025-06-11 19:18:44 +0530

    valid changes

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './styles/UserList.css';
+import './styles/AddUser.css';
 
 const UserList = ({ token }) => {
   const [users, setUsers] = useState([]);
